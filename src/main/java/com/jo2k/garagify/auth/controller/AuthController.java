13f149433708eb5c717cedec6fcb84cbe1e225c8
Karol Zając <karolzajac.0407@gmail.com>
2025-05-17 16:08:41 +0200

    feat: add SSO

--- src/main/java/com/jo2k/garagify/auth/controller/AuthController.java
+++ src/main/java/com/jo2k/garagify/auth/controller/AuthController.java
@@ -1,29 +1,28 @@
 package com.jo2k.garagify.auth.controller;
 
+import com.jo2k.api.AuthControllerApi;
 import com.jo2k.dto.TokenDto;
 import com.jo2k.dto.TokenVerificationForm;
 import com.jo2k.garagify.auth.service.AuthService;
 import com.jo2k.garagify.auth.service.GoogleTokenVerifierService;
 import com.jo2k.garagify.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/auth")
-class AuthController {
+class AuthController implements AuthControllerApi {
 
     private final GoogleTokenVerifierService tokenVerifierService;
     private final AuthService authService;
     private final UserService userService;
 
 
-    @PostMapping("/access")
-    TokenDto authenticateWithGoogle(@RequestBody TokenVerificationForm body) {
+    @Override
+    public ResponseEntity<TokenDto> authenticateWithGoogle(@RequestBody TokenVerificationForm tokenVerificationForm) {
         // TODO replace with real verification for deployment with real google client id
 
         /*   var payload = tokenVerifierService.verifyToken(idToken);
@@ -33,13 +32,14 @@ class AuthController {
         /* For now any token with "email" claim will go through, for example:
          * this token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJlbWFpbCI6Im15QGVtYWlsIn0.qqei_kFbDs8ALnBaOwqXIDg7n-F7sfp4FT_yXmDYLy0
          */
-        String email = tokenVerifierService.verifyTokenMock(body.getToken());
-        String userId = userService.findOrCreateByEmail(email).toString();
-        return authService.generateToken(userId);
+        String email = tokenVerifierService.verifyTokenMock(tokenVerificationForm.getToken());
+        TokenDto tokenDto = authService.generateToken(userService.findOrCreateByEmail(email).toString());
+        return ResponseEntity.ok(tokenDto);
     }
 
-    @PostMapping("/refresh")
-    TokenDto refresh(@RequestBody TokenVerificationForm body) {
-        return authService.getRefreshToken(body.getToken());
+    @Override
+    public ResponseEntity<TokenDto> refreshToken(@RequestBody TokenVerificationForm tokenVerificationForm) {
+        TokenDto tokenDto = authService.getRefreshToken(tokenVerificationForm.getToken());
+        return ResponseEntity.ok(tokenDto);
     }
 }
