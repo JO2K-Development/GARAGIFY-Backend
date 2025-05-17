21376447df6952fdaafb680706136bec3312d09e
Karol Zając <karolzajac.0407@gmail.com>
2025-05-17 15:49:19 +0200

    feat: add SSO

---
+++ src/main/java/com/jo2k/garagify/common/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jo2k.garagify.common.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
