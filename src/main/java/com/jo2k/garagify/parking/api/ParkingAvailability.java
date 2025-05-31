0f207f48889768e8185d335baa466736caa2c16f
Karol Zając <karolzajac.0407@gmail.com>
2025-05-31 23:48:42 +0200

    feat: endpoints

---
+++ src/main/java/com/jo2k/garagify/parking/api/ParkingAvailability.java
@@ -0,0 +1,13 @@
+package com.jo2k.garagify.parking.api;
+
+import com.jo2k.dto.TimeRangeDto;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface ParkingAvailability {
+    List<TimeRangeDto> getTimeRanges(Integer parkingId, OffsetDateTime untilWhen);
+
+    List<UUID> getSpots(Integer parkingId, OffsetDateTime from, OffsetDateTime until);
+}
